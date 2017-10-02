1fb2ea7bdbb9837c0f1ab26e7e6da818b162ea15
Thomas <Thomas.laine25@gmail.com>
2017-10-02 12:36:22 +0200

    Added ConversationInitializer

---
+++ OSWALD/DAL/ConversationInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using OSWALD.Models;
+
+namespace OSWALD.DAL
+{
+    public class ConversationInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ConversationContext>
+    {
+        protected override void Seed(ConversationContext context)
+        {
+            var conversations = new List<Conversation>
+            {
+                new Conversation{Id = 0, ConversationDate = DateTime.Now, User = "Hello", Bot = "Oswin" }
+            };
+
+            conversations.ForEach(c => context.Conversations.Add(c));
+            context.SaveChanges();
+        }
+    }
+}
