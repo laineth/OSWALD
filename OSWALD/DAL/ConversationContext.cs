20ebf4eed1f075bd1e8e41f3ede2c2cb745c06ef
Thomas <Thomas.laine25@gmail.com>
2017-10-02 12:26:55 +0200

    added Data Access Layer folder and DbContext for a Conversation

---
+++ OSWALD/DAL/ConversationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using OSWALD.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace OSWALD.DAL
+{
+    public class ConversationContext : DbContext
+    {
+        public ConversationContext() : base("ConversationContext")
+        {
+        }
+
+        public DbSet<Conversation> Conversations { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
