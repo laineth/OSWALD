8f6cc3b6e67958ab6e10be37983439ba57d59e51
Thomas <Thomas.laine25@gmail.com>
2017-10-03 14:37:39 +0200

    updated sendRequest fle

--- OSWALD/Scripts/sendRequest.js
+++ OSWALD/Scripts/sendRequest.js
@@ -1,7 +1,16 @@
-﻿$("#send").click(function()
+﻿$(document).ready(function()
 {
-    $.get("http://localhost:54890/bot", function (data, status)
+    var botName = "Oswin";
+    var punctuation = ": ";
+    var name = botName + punctuation;
+
+    document.getElementById('chat').value = name + 'I am here, to help you gather information about Thomas.';
+
+    $("#send").click(function()
     {
-        alert("Data: " + data + "\nStatus: " + status);
-    }, "json");
+        $.get("http://localhost:54890/bot", function (data, status)
+        {
+            alert("Data: " + data + "\nStatus: " + status);
+        }, "json");
+    });
 });
